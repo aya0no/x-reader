8ab6e82f88b11c461466b826bef04dda80f7754a
aya0no <l.carvings.i@gmail.com>
2026-08-08 09:18:45 +0900

    Bump userscript metadata to 7.0.4.12.7

--- x-reader.meta.js
+++ x-reader.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         X Reader Accordion v7.0.4-step11-light
 // @namespace    local.x-reader-accordion
-// @version      7.0.4.12.6
-// @description  件数表示を省いたシンプルな固定リストタブのモノクロ軽量版です。
+// @version      7.0.4.12.7
+// @description  ホーム、1、2、株、レシピを固定タブで切り替えるモノクロ軽量版です。
 // @match        https://x.com/*
 // @match        https://twitter.com/*
 // @updateURL    https://raw.githubusercontent.com/aya0no/x-reader/main/x-reader.meta.js
