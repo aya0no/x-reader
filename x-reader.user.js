1cd3a514296b19c3852b83eb05666c476abc7113
aya0no <l.carvings.i@gmail.com>
2026-08-21 00:20:18 +0900

    Speed up the X Reader startup screen

--- x-reader.user.js
+++ x-reader.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         X Reader Accordion v7.0.4-step11-light
 // @namespace    local.x-reader-accordion
-// @version      7.0.4.13.2
-// @description  専用の起動画面、表示切替、左右スワイプに対応したX Readerです。
+// @version      7.0.4.13.3
+// @description  高速な専用起動画面、表示切替、左右スワイプに対応したX Readerです。
 // @match        https://x.com/*
 // @match        https://twitter.com/*
 // @updateURL    https://raw.githubusercontent.com/aya0no/x-reader/main/x-reader.meta.js
 // @downloadURL  https://raw.githubusercontent.com/aya0no/x-reader/main/x-reader.user.js
 // @run-at       document-start
+// @inject-into  content
+// @weight       999
+// @noframes
 // @grant        none
 // ==/UserScript==
 
